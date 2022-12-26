86f9363d26b90e5899fbce9c0a153a027a5bbf3a
ChoiMyungRak <audfkr0808@naver.com>
2022-12-27 00:25:06 +0900

    데이터 추가

---
+++ app/src/main/java/com/lakue/Sample.kt
@@ -0,0 +1,29 @@
+package com.lakue
+
+import com.lakue.domain.TrainDto
+
+val trainInfo = arrayListOf(
+    TrainDto("1호선", "1577-1234"),
+    TrainDto("2호선", "1577-1234"),
+    TrainDto("3호선", "1577-1234"),
+    TrainDto("4호선", "1577-1234"),
+    TrainDto("5호선", "1577-1234"),
+    TrainDto("6호선", "1577-1234"),
+    TrainDto("7호선", "1577-1234"),
+    TrainDto("8호선", "1577-1234"),
+    TrainDto("9호선", "1544-4009"),
+    TrainDto("신분당선", "031-8018-7777"),
+    TrainDto("공항철도", "1544-7769"),
+    TrainDto("경의중앙선", "1544-7769"),
+    TrainDto("분당선", "1544-7769"),
+)
+
+val messate = arrayListOf(
+    "너무 더워요. 에어컨좀 틀어주세요!",
+    "너무 추워요. 히터좀 틀어주세요!",
+    "너무 더워요. 에어컨좀 줄여주세요!",
+    "너무 추워요. 히터좀 줄여주세요!",
+    "취객이 불편을 줘요",
+    "여기 진상이있어요",
+    "기타"
+)
---
+++ app/src/main/java/com/lakue/domain/TrainDto.kt
@@ -0,0 +1,6 @@
+package com.lakue.domain
+
+data class TrainDto(
+    val name: String,
+    val phone: String
+)
