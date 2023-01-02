1f2ee4b2e76916eac49eaa0d8d294829d7ad04b2
ChoiMyungRak <audfkr0808@naver.com>
2023-01-03 02:06:41 +0900

    문자 보내는 기능 추가

--- app/src/main/java/com/lakue/trainmessage/MainActivity.kt
+++ app/src/main/java/com/lakue/trainmessage/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.lakue.trainmessage
 
+import android.app.PendingIntent
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -12,6 +14,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.lakue.domain.TrainDto
@@ -20,7 +23,7 @@ import com.lakue.trainmessage.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    var trainInfo: TrainDto? = null
+    private var trainInfo: TrainDto? = null
 
     private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
         if(it.resultCode == RESULT_OK){
@@ -66,7 +69,37 @@ class MainActivity : AppCompatActivity() {
                     )
                 )
             }
+            btnSendSms.setOnClickListener {
+                if(trainInfo == null || etTrainNumber.text.isNullOrBlank()) {
+                    showToast("열차 정보를 입력해주세요.")
+                    return@setOnClickListener
+                }
+                if(etMessage.text.isNullOrBlank()) {
+                    showToast("문의 내용을 입력해주세요.")
+                    return@setOnClickListener
+                }
+
+                val text = """
+${trainInfo?.name}
+열차번호 : ${etTrainNumber.text}
+문의내용 : ${etMessage.text}
+                """.trimIndent()
+                sendSMS(
+                    trainInfo?.phone ?: "",
+                    text
+                )
+            }
         }
     }
 
+    private fun sendSMS(phone: String, message: String) {
+        val sendIntent = Intent(Intent.ACTION_VIEW)
+        sendIntent.setData(Uri.parse("sms:$phone"))
+        sendIntent.putExtra("sms_body", message)
+        startActivity(sendIntent)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
 }
